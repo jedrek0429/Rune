d047975933cf12852aa0f6abbc9b127a2dce2b02
Andrzej Bansleben <a.bansleben0429@gmail.com>
2026-08-30 15:49:37 +0200

    feat(images): build all compiler and runtime images

--- firecracker/build-all.sh
+++ firecracker/build-all.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for language in javascript python rust; do
-  ./firecracker/build-rootfs.sh "$language"
-  ./firecracker/build-snapshot.sh "$language"
+for runtime in native python ruby; do
+  ./firecracker/build-rootfs.sh invocation "$runtime"
+  ./firecracker/build-snapshot.sh "$runtime"
+done
+
+for pool in scriptc clang rust dotnet-aot python ruby; do
+  ./firecracker/build-rootfs.sh build "$pool"
 done
