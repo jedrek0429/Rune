43b5ab849216ad6a94e53cb5586e5e03f3b9da87
Andrzej Bansleben <a.bansleben0429@gmail.com>
2026-08-30 13:41:32 +0200

    feat(firecracker): define VM resource profiles

---
+++ firecracker/resource-policy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+rune_invocation_profile() {
+  case "${1:-}" in
+    native) echo "1 192 32 3 32 128" ;;
+    python|ruby) echo "1 256 32 3 32 128" ;;
+    *) return 2 ;;
+  esac
+}
+
+rune_build_profile() {
+  case "${1:-}" in
+    scriptc) echo "1 512 512 30 128 256" ;;
+    clang) echo "1 512 512 20 128 256" ;;
+    rust) echo "2 1024 512 45 128 256" ;;
+    dotnet-aot) echo "2 2048 768 60 128 256" ;;
+    python|ruby) echo "1 512 256 20 128 256" ;;
+    *) return 2 ;;
+  esac
+}
