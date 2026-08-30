128836a64ba31461a8e77785f579e83a0e08dccb
Andrzej Bansleben <a.bansleben0429@gmail.com>
2026-08-30 18:13:45 +0200

    refactor: build one Rune execution image

--- firecracker/build-all.sh
+++ firecracker/build-all.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for runtime in native python ruby; do
-  ./firecracker/build-rootfs.sh invocation "$runtime"
-  ./firecracker/build-snapshot.sh "$runtime"
-done
+./firecracker/build-rootfs.sh invocation rune
+./firecracker/build-snapshot.sh
 
 for pool in scriptc clang rust dotnet-aot python ruby; do
   ./firecracker/build-rootfs.sh build "$pool"
