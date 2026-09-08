e3dd4ee97570937d3d82e8f6bd9d9e783bd26e48
Andrzej Bansleben <a.bansleben0429@gmail.com>
2026-09-08 18:30:54 +0200

    refactor: cut bot over to Firecracker runtime

--- src/Rune.Bot/Program.cs
+++ src/Rune.Bot/Program.cs
@@ -6,10 +6,8 @@ using NetCord.Hosting.Gateway;
 using NetCord.Hosting.Services;
 using NetCord.Hosting.Services.ApplicationCommands;
 using Rune.Bot;
-using Rune.Bot.Host;
 using Rune.Core.Runes;
 using Rune.Runtime;
-using Rune.Runtime.Compilation;
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -25,61 +23,18 @@ builder.Services
     .AddApplicationCommands()
     .AddGatewayHandlers(typeof(Program).Assembly);
 
-
 builder.Services
     .AddSingleton<RuneRegistry>()
     .AddSingleton<RuneService>()
     .AddSingleton<RuneUploadReader>()
-    .AddRuneCompilation(options =>
-    {
-        var home =
-            Environment.GetFolderPath(
-                Environment.SpecialFolder.UserProfile);
-
-        options.JavaScriptCompiler =
-            Path.Combine(
-                home,
-                ".local/bin/extism-js");
-
-        options.PythonCompiler =
-            Path.Combine(
-                home,
-                ".local/bin/extism-py");
-
-        options.RustCompiler = "cargo";
-
-        options.JavaScriptTimeout =
-            TimeSpan.FromSeconds(30);
-
-        options.PythonTimeout =
-            TimeSpan.FromSeconds(30);
-
-        options.RustTimeout =
-            TimeSpan.FromMinutes(2);
-
-        options.RustTargetDirectory =
-            Path.Combine(
-                Path.GetTempPath(),
-                "rune-rust-target");
-    })
     .AddRuneRuntime(options =>
     {
-        options.ExecutionTimeout =
-            TimeSpan.FromSeconds(2);
-
-        options.MaxMemoryPages = 4096;
-        options.MaxConcurrentExecutions = 16;
-        options.MaxHostRequestsPerInvocation = 32;
-        options.MaxReplyLength = 2000;
+        options.RedisConnectionString =
+            Environment.GetEnvironmentVariable("RUNE_REDIS_URL") ?? "localhost:6379";
     })
-    .AddSingleton<RuneEventDispatcher>()
-    .AddSingleton<
-        IRuneHostRequestHandler,
-        NetCordRuneHostRequestHandler>();
+    .AddSingleton<RuneEventDispatcher>();
 
 var host = builder.Build();
-
-var registry = host.Services.GetRequiredService<RuneRegistry>();
 
 host.AddModules(typeof(Program).Assembly);
 
